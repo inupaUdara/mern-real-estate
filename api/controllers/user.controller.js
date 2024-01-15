b8908bdbbdce9057db9deb8f4a4725b73067ae26
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-01-15 23:44:25 +0530

    create get user listing api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 export const test = (req, res) => {
     res.json({
          message: "test route" 
@@ -41,5 +42,19 @@ export const deleteUser = async (req, res, next) => {
         res.status(200).json("User has been deleted...");
     } catch (error) {
         next(error);
+    }
+}
+
+export const getUserListing = async (req, res, next) => {
+    if(req.user.id === req.params.id){
+        try {
+            const listing = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listing);
+        } catch (error) {
+            next(error);
+        }
+    }
+    else{
+        return next(errorHandler(401, "You can only get your own account!"));
     }
 }
