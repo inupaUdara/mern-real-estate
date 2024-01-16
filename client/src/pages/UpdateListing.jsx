8816f32f94f48b17dea9fad462a2788f9eccb9e1
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-01-17 01:51:08 +0530

    Complete listing item card

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -363,7 +363,7 @@ export default function UpdateListing() {
             className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95
         disabled:opacity-80"
           >
-            {loading ? "Loading..." : "Update Listning"}
+            {loading ? "Updating..." : "Update Listning"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
