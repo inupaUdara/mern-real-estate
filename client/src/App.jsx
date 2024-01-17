8bf5a13962f094566befb6ebf37dc443b3f0f017
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-01-17 15:07:36 +0530

    fix app.jsx file for create listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Profile from "./pages/Profile";
 
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./pages/createListing";
+import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
