3ea3aa279d31ac27b499346c8abbe9f6efd3f846
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-01-18 12:23:15 +0530

    modified

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,7 +49,7 @@ export default function Home() {
       {/* {top} */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          FInd your next <span className="text-slate-500">perfect</span> <br />place with ease
+          Find your next <span className="text-slate-500">perfect</span> <br />place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
         Udara Estate will help you finf your next perfect place with ease. <br /> 
